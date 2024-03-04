1fe0a0e1cca4312fc65016111791d3ab8beaa35f
ramadsijiv <damar.jati@karirlab.co>
2024-03-04 18:00:57 +0700

    remove console

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const app = require('./app');
 const PORT =  process.argv[2] || 8080
 
 app.listen(PORT, () => {
-    console.log("New Workflow");
     console.log(`Server listening on port ${PORT}`);
 });
