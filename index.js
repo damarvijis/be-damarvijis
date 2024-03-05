6bb486389a99e7fc52341ee8038e0306fb36b98a
ramadsijiv <damar.jati@karirlab.co>
2024-03-06 00:32:39 +0700

    feat(env): add dotenv file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const app = require('./app');
-const PORT =  process.argv[2] || 8080
+const PORT =  process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
